c4c71bca8d3f8e8e4b72b435dcd71b647e4663ce
Akshay <acharyaakshay445@gmail.com>
2025-06-17 16:08:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-04b4f1a9cf54c11d0"  # Specify an appropriate AMI ID
+    ami           = "ami-020cba7c55df1f615"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0ffa1a1f762804b4d"
+    key_name = 'nodes"
 }
